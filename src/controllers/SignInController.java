6de5766055cdfafd1815f1bd29342e8f739a6a24
osama-abuhamdan <osamaabuhamdan@yahoo.com>
2019-04-06 10:23:44 +0300

    Initial commit

---
+++ src/controllers/SignInController.java
@@ -0,0 +1,64 @@
+package controllers;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class SignInController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private PasswordField passowrdField;
+
+    @FXML
+    private TextField userNameField;
+
+    @FXML
+    void aboutClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clearFieldsButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void closeClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void signInButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void backButtonClicked(ActionEvent event) throws IOException {
+        Parent loader = FXMLLoader.load(getClass().getResource("/fxmls/startPage.fxml"));//Creates a Parent called loader and assign it as ScReen2.FXML
+        Scene scene = new Scene(loader); //This creates a new scene called scene and assigns it as the Sample.FXML document which was named "loader"
+        Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow(); //this accesses the window.
+        app_stage.setScene(scene); //This sets the scene as scene
+        app_stage.show(); // this shows the scene
+    }
+    @FXML
+    void initialize() {
+        assert passowrdField != null : "fx:id=\"passowrdField\" was not injected: check your FXML file 'SignIn.fxml'.";
+        assert userNameField != null : "fx:id=\"userNameField\" was not injected: check your FXML file 'SignIn.fxml'.";
+
+    }
+}
---
+++ src/controllers/StartPageController.java
@@ -0,0 +1,63 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class StartPageController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void aboutClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void closeClicked(ActionEvent event) {
+        System.exit(0);
+
+    }
+
+    @FXML
+    void signInClicked(ActionEvent event)  {
+        try{
+            Parent loader = FXMLLoader.load(getClass().getResource("/fxmls/signIn.fxml"));//Creates a Parent called loader and assign it as ScReen2.FXML
+            Scene scene = new Scene(loader); //This creates a new scene called scene and assigns it as the Sample.FXML document which was named "loader"
+            Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow(); //this accesses the window.
+            app_stage.setScene(scene); //This sets the scene as scene
+            app_stage.show(); // this shows the scene
+        }
+        catch (Exception e){e.printStackTrace();}
+    }
+
+    @FXML
+    void signUpClicked(ActionEvent event){
+        try{
+            Parent loader = FXMLLoader.load(getClass().getResource("/fxmls/signUp.fxml"));//Creates a Parent called loader and assign it as ScReen2.FXML
+            Scene scene = new Scene(loader); //This creates a new scene called scene and assigns it as the Sample.FXML document which was named "loader"
+            Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow(); //this accesses the window.
+            app_stage.setScene(scene); //This sets the scene as scene
+            app_stage.show(); // this shows the scene
+        }
+        catch (Exception e){e.printStackTrace();}
+    }
+
+    @FXML
+    void initialize() throws IOException {
+    }
+}
