e009cb0c89c9c52488873f710461a19554d85bc6
osama-abuhamdan <osamaabuhamdan@yahoo.com>
2019-04-21 01:46:47 +0300

    GUI continue ...

---
+++ src/Objects/Challenge.java
@@ -0,0 +1,18 @@
+package Objects;
+
+
+import java.io.File;
+import java.time.LocalTime;
+import java.util.List;
+
+public class Challenge {
+    private String time;
+    private String relatedWord;
+    private List<File> images;
+
+    public Challenge(String time, String relatedWord, List<File> images) {
+        this.time = time;
+        this.relatedWord = relatedWord;
+        this.images = images;
+    }
+}
--- src/controllers/CreateChallengeController.java
+++ src/controllers/CreateChallengeController.java
@@ -1,15 +1,29 @@
 package controllers;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import Objects.Challenge;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableView;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
+import javax.swing.*;
 
 public class CreateChallengeController {
 
@@ -20,10 +34,16 @@ public class CreateChallengeController {
     private URL location;
 
     @FXML
-    private TableView<?> photosNamesTable;
+    private TextField relatedWord;
 
     @FXML
-    private TextField relatedWord;
+    private TextField time;
+
+    List<File> files = new ArrayList<>();
+
+    @FXML
+    private ListView<String> filesList;
+
 
     @FXML
     void aboutClicked(ActionEvent event) {
@@ -31,13 +51,23 @@ public class CreateChallengeController {
     }
 
     @FXML
-    void backButtonClicked(ActionEvent event) {
+    void backButtonClicked(ActionEvent event) throws IOException {
+        Parent loader = FXMLLoader.load(getClass().getResource("/fxmls/homePage.fxml"));//Creates a Parent called loader and assign it as ScReen2.FXML
+        Scene scene = new Scene(loader); //This creates a new scene called scene and assigns it as the Sample.FXML document which was named "loader"
+        Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow(); //this accesses the window.
+        app_stage.setScene(scene); //This sets the scene as scene
+        app_stage.show(); // this shows the scene
 
     }
 
     @FXML
     void browseButtonClicked(ActionEvent event) {
-
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Choose 4 photos");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Images", "*.png", "*.jpg", "*.jpeg", "*.gif"));
+        files = fileChooser.showOpenMultipleDialog(new Stage());
+        viewFilesNames(files);
     }
 
     @FXML
@@ -67,13 +97,27 @@ public class CreateChallengeController {
 
     @FXML
     void submitButtonClicked(ActionEvent event) {
-
+        Challenge challenge = new Challenge(time.getText(), relatedWord.getText(), files);
+        JOptionPane.showMessageDialog(null, " Challenge submitted successfully\nPress back or create new Challenge");
+        relatedWord.clear();
+        time.clear();
+        filesList.setItems(null);
     }
 
     @FXML
     void initialize() {
-        assert photosNamesTable != null : "fx:id=\"photosNamesTable\" was not injected: check your FXML file 'Start Page.fxml'.";
-        assert relatedWord != null : "fx:id=\"relatedWord\" was not injected: check your FXML file 'Start Page.fxml'.";
+        assert relatedWord != null : "fx:id=\"relatedWord\" was not injected: check your FXML file 'uploadPhotos.fxml'.";
+        assert filesList != null : "fx:id=\"filesList\" was not injected: check your FXML file 'uploadPhotos.fxml'.";
 
+    }
+
+    private void viewFilesNames(List<File> files) {
+        List<String> fileNames = new ArrayList<>();
+        for ( File file : files ) {
+            fileNames.add(file.getName());
+        }
+        ObservableList<String> observableList = FXCollections.observableArrayList(fileNames);
+        FilteredList<String> filteredList = new FilteredList<>(observableList, s -> true);
+        filesList.setItems(filteredList);
     }
 }
